b50ec2b24829900504ca761aa343922c3a3193d0
LelioMarcos <leliomrcunha@gmail.com>
2023-04-05 21:45:05 -0300

    initial commit

---
+++ server.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, render_template_string, request
+
+app = Flask(__name__)
+seguro = "nao"
+
+@app.route("/")
+def root(user=None):
+    return "It's working"
+
+@app.route("/<user>")
+def hello_world(user=None):
+    return render_template('index.html', user=user)
+
+# Exemplo de código MUITO inseguro.
+@app.route("/produtos")
+def search():
+    param = request.args.get('search') or None
+    template = """
+        <html>
+            <head>
+                <title>Produtos</title>
+            </head>
+            <body>
+                <h1>Resultados para """ + param + """</h1>
+                <p> Nenhum resultado :(</p>
+            </body>
+        </html>
+    """
+    return render_template_string(template)
