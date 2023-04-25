da2662f302d39d96cfb52d5e4202e7fbae83201c
LelioMarcos <leliomrcunha@gmail.com>
2023-04-25 11:07:12 -0300

    Sistema de mensagens para demonstração

--- server.py
+++ server.py
@@ -1,7 +1,14 @@
-from flask import Flask, render_template, render_template_string, request
+from flask import Flask, render_template, render_template_string, request, redirect, url_for
+import json
 
 app = Flask(__name__)
-seguro = "nao"
+
+notes = {
+    "lelio": ["fui ao mercado hoje", "mercado:\nsuco\nrefri\nmiojo"],
+    "angus": ["senha do banco: GANESH\{tempalte_injection\}"]
+}
+
+session = {}
 
 @app.route("/")
 def root(user=None):
@@ -15,6 +22,55 @@ def hello_world(user=None):
 @app.route("/insecure/<user>")
 def hello_world_insecure(user=None):
     return render_template('index.html', user=render_template_string(user))
+
+@app.route("/messages", methods=['GET', 'POST'])
+def messages():
+    if session and session['username']:
+        print(notes[session['username']])
+
+        template = ""
+        
+        for message in notes[session['username']]:
+            template += message + ","
+
+        template = template[:-1]
+
+        return render_template('passwords.html', user=session['username'], messages=render_template_string(template))
+    
+    return redirect('/login')
+
+# Criar rota de adicionar
+@app.route("/messages/add", methods=["POST"])
+def add_message():
+    if session['username']:
+        if request.form['message']:
+            notes[session['username']].append(request.form['message'])
+
+            return redirect(url_for('messages'))
+
+        return "Faltam parâmetros"
+
+    return redirect('/login')
+
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        f = open("usuarios.json")
+        data = json.load(f)
+        f.close()
+        for i in range(len(data)):
+            if data[i]["username"] == request.form['username']:
+                if data[i]["password"] == request.form['password']:
+                    session['username'] = data[i]["username"]
+                    return redirect('/messages')
+                else: 
+                    return "Erro ao logar"
+                break
+    else:
+        return render_template('login.html')
+        
 
 # Exemplo de código MUITO inseguro.
 @app.route("/produtos")
